55a8f8c6bde76e068e21a7eda86ebe316107e013
Camille Archambault <c.archambault01@gmail.com>
2018-02-22 11:11:22 -0500

    Début du tp avec l'exercice 7

---
+++ mes_modules/peupler/tableaux.js
@@ -0,0 +1,112 @@
+"use strict"
+let prenom = [
+            'Luc',
+            'Éric',
+            'François',
+            'Lyne',
+            'carolle',
+            'Sylvie',
+            'Renée',
+            'Mathieu',
+            'Eddy',
+            'Jean',
+            'Paul',
+            'Gustave',
+            'Rose-Lyne',
+            'Rose-Lise',
+            'Rose',
+            'Xavier',
+            'Olivier',
+            'Michel',
+            'Jean-François',
+            'Bob',
+            'Gil',
+            'Martin',
+            'Pierre',
+            'Maxime',
+            'Léo',
+            'Léo-Paul',
+            'Paul',
+            'Alix',
+            'Alixe',
+            'Chantal',
+            'Christelle',
+            'Katy',
+            'Johanne',
+            'Béatrice',
+            'Noémie',
+            'Coralie',
+            'Pénéloppe',
+            'Charlotte'
+         ];
+
+let nom = [
+            'Martin',
+            'Marchand',
+            'Trudeau',
+            'Lafontaine',
+            'Gravel',
+            'Huberdeau',
+            'Massé',
+            'St-Pierre',
+            'Dandurand',
+            'Hamel',
+            'Larose',
+            'Ambroise',
+            'Kuvel',
+            'Lamarre',
+            'Sorel',
+            'Frapier',
+            'Archer',
+            'Dejoie',
+            'Moïse',
+            'Fortin',
+            'Stien',
+            'Gagnon',
+            'Chédiac',
+            'Roy',
+            'Gagner',
+            'Larue',
+            'Lafortune',
+            'Roffi',
+            'Cartier',
+            'Maisonneuve',
+            'Levesque',
+            'Talon',
+            'Bellemarre',
+            'Mercier'
+         ];
+
+let telephone = [
+            '514-555-5555',
+            '450-655-4134',
+            '514-632-6532',
+            '514-888-4444',
+            '438-853-7777',
+            '514-522-5674',
+            '450-632-9974',
+            '450-742-3456',
+            '438-632-6632',
+            '514-658-1246'
+         ]; 
+
+let courriel = [
+
+            'hotmail.com',
+                  'vedeotron.com',
+                  'gmail.com',
+                  'yahoo.com',
+                  'cmaisonneuve.qc.ca',
+                  'webfaction.com',
+                  'cvsyc.com',
+                  'bell.ca',
+                  'umontreal.ca',
+                  'skillscanada.ca',
+                  'inforoutefpt.com'
+
+         ];
+
+module.exports.prenom = prenom;
+module.exports.nom = nom;
+module.exports.telephone = telephone;
+module.exports.courriel = courriel;
