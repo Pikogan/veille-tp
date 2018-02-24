01c1ee0900404eaa6863049c744bd1d678478536
ccc aaa <c.archambault01@gmail.com>
2018-02-23 22:43:59 -0500

    Réparation de trier, essai de mettre css à trier

--- app_01.js
+++ app_01.js
@@ -18,7 +18,6 @@ app.get('/membres', (req, res) => {
     let cursor = db.collection('adresse').find().toArray(function(err, resultat){
 
          if (err) return console.log(err)
-         //console.log('util = ' + util.inspect(resultat));
 
          //transfert du contenu vers la vue index.ejs (renders)
          //affiche le contenu de la BD
